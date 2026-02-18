a45a4472a88fd6b88913cd74b6835d4dee90d5e0
Brauhaus <brauhaus05@gmail.com>
2026-02-17 20:32:19 -0800

    fix: Update hero image source path to use absolute URL.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ export const Hero: React.FC = () => {
       {/* Image Section */}
       <section className="relative w-full h-[60vh] md:h-[80vh] bg-[#7C8356]">
         <img 
-          src="./assets/vero.png" 
+          src="/assets/vero.png" 
           alt="Vero Vargas smiling" 
           className="w-full h-full object-cover object-top"
         />
